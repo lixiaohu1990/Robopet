ddc3593a3c3abecef760f8411f532e9c65f7555c
Pat Sluth <pat.sluth@gmail.com>
2016-01-24 11:07:42 -0600

    added particle effects to walls (Roll Mini Game)

--- RoboPet/RBPMiniGameScene_Roll.m
+++ RoboPet/RBPMiniGameScene_Roll.m
@@ -65,42 +65,71 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
     // Start in centre
     self.player.position = CGPointMake(self.size.width / 2, self.size.height / 2);
     [self addChild:self.player];
-    
-    
-    // Setup walls
-    SKSpriteNode *leftWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(1, self.size.height)];
-    leftWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:leftWall.size];
-    leftWall.physicsBody.dynamic = NO;
-    leftWall.physicsBody.categoryBitMask = CollisionCategoryWall;
-    leftWall.physicsBody.affectedByGravity = NO;
-    leftWall.position = CGPointMake(-leftWall.size.width, self.size.height / 2);
-    [self addChild:leftWall];
-    
-    SKSpriteNode *rightWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(1, self.size.height)];
-    rightWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rightWall.size];
-    rightWall.physicsBody.dynamic = NO;
-    rightWall.physicsBody.categoryBitMask = CollisionCategoryWall;
-    rightWall.physicsBody.affectedByGravity = NO;
-    rightWall.position = CGPointMake(self.size.width + rightWall.size.width, self.size.height / 2);
-    [self addChild:rightWall];
-    
-    SKSpriteNode *topWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.size.width, 1)];
-    topWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:topWall.size];
-    topWall.physicsBody.dynamic = NO;
-    topWall.physicsBody.categoryBitMask = CollisionCategoryWall;
-    topWall.physicsBody.affectedByGravity = NO;
-    topWall.position = CGPointMake(self.size.width / 2, self.size.height + topWall.size.height);
-    [self addChild:topWall];
-    
-    SKSpriteNode *bottomWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.size.width, 1)];
-    bottomWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomWall.size];
-    bottomWall.physicsBody.dynamic = NO;
-    bottomWall.physicsBody.categoryBitMask = CollisionCategoryWall;
-    bottomWall.physicsBody.affectedByGravity = NO;
-    bottomWall.position = CGPointMake(self.size.width / 2, -bottomWall.size.height);
-    [self addChild:bottomWall];
-    
+	
+	[self setupWalls];
+	
     [self generatePickup];
+}
+
+- (void)setupWalls
+{
+	SKSpriteNode *leftWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(1, self.size.height)];
+	leftWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:leftWall.size];
+	leftWall.physicsBody.dynamic = NO;
+	leftWall.physicsBody.categoryBitMask = CollisionCategoryWall;
+	leftWall.physicsBody.affectedByGravity = NO;
+	leftWall.position = CGPointMake(-leftWall.size.width, self.size.height / 2);
+	[self addChild:leftWall];
+	
+	SKSpriteNode *rightWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(1, self.size.height)];
+	rightWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rightWall.size];
+	rightWall.physicsBody.dynamic = NO;
+	rightWall.physicsBody.categoryBitMask = CollisionCategoryWall;
+	rightWall.physicsBody.affectedByGravity = NO;
+	rightWall.position = CGPointMake(self.size.width + rightWall.size.width, self.size.height / 2);
+	[self addChild:rightWall];
+	
+	SKSpriteNode *topWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.size.width, 1)];
+	topWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:topWall.size];
+	topWall.physicsBody.dynamic = NO;
+	topWall.physicsBody.categoryBitMask = CollisionCategoryWall;
+	topWall.physicsBody.affectedByGravity = NO;
+	topWall.position = CGPointMake(self.size.width / 2, self.size.height + topWall.size.height);
+	[self addChild:topWall];
+	
+	SKSpriteNode *bottomWall = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.size.width, 1)];
+	bottomWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bottomWall.size];
+	bottomWall.physicsBody.dynamic = NO;
+	bottomWall.physicsBody.categoryBitMask = CollisionCategoryWall;
+	bottomWall.physicsBody.affectedByGravity = NO;
+	bottomWall.position = CGPointMake(self.size.width / 2, -bottomWall.size.height);
+	[self addChild:bottomWall];
+	
+	
+	// Setup particles
+	SKEmitterNode *leftWallParticles = [NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"Sparks" ofType:@"sks"]];
+	leftWallParticles.position = leftWall.position;
+	leftWallParticles.zRotation = M_PI_2;
+	leftWallParticles.targetNode = self.scene;
+	[self addChild:leftWallParticles];
+	
+	SKEmitterNode *rightWallParticles = [NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"Sparks" ofType:@"sks"]];
+	rightWallParticles.position = rightWall.position;
+	rightWallParticles.zRotation = M_PI_2;
+	rightWallParticles.targetNode = self.scene;
+	[self addChild:rightWallParticles];
+	
+	SKEmitterNode *topWallParticles = [NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"Sparks" ofType:@"sks"]];
+	topWallParticles.position = topWall.position;
+	topWallParticles.zRotation = M_PI;
+	topWallParticles.targetNode = self.scene;
+	[self addChild:topWallParticles];
+	
+	SKEmitterNode *bottomWallParticles = [NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"Sparks" ofType:@"sks"]];
+	bottomWallParticles.position = bottomWall.position;
+	bottomWallParticles.zRotation = M_PI;
+	bottomWallParticles.targetNode = self.scene;
+	[self addChild:bottomWallParticles];
 }
 
 #pragma mark - RBPMiniGameScene_Roll
@@ -110,12 +139,23 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
  */
 - (void)generatePickup
 {
+	//TODO: check distance from player
+	
     // Instantiate pickup
     SKSpriteNode *pickup = [SKSpriteNode spriteNodeWithColor:[UIColor greenColor] size:CGSizeMake(100, 100)];
-    
+	CGSize pickupHalfSize = CGSizeMake(pickup.size.width / 2, pickup.size.height / 2);
+	
     // Generate random position
-    CGFloat xPosition = arc4random_uniform(self.size.width); // +- pickup half width +- padding
-    CGFloat yPosition = arc4random_uniform(self.size.height); // +- pickup half height +- padding
+	// TODO: padding?
+	CGFloat xPosition = arc4random_uniform(self.size.width);
+	CGFloat yPosition = arc4random_uniform(self.size.height);
+	// Clamp positions so pickup is never offscreen
+	xPosition = MIN(xPosition, self.size.width - pickupHalfSize.width);
+	xPosition = MAX(xPosition, pickupHalfSize.width);
+	yPosition = MIN(yPosition, self.size.height - pickupHalfSize.height);
+	yPosition = MAX(yPosition, pickupHalfSize.height);
+	
+	
     pickup.position = CGPointMake(xPosition, yPosition);
     
     // Add to scene
@@ -128,14 +168,14 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
 - (void)update:(CFTimeInterval)currentTime
 {
     // Apply impulse force in the opposite direction of the gyroscope for precise deceleration control
-    CGVector impulseVector = CGVectorMake(-self.motion.gyroData.rotationRate.x, -self.motion.gyroData.rotationRate.y);
+    CGVector impulseVector = CGVectorMake(self.motion.gyroData.rotationRate.x, self.motion.gyroData.rotationRate.y);
     
-    if (ABS(impulseVector.dx) < 0.4) { impulseVector.dx = 0.0; }
-    if (ABS(impulseVector.dy) < 0.4) { impulseVector.dy = 0.0; }
+    if (ABS(impulseVector.dx) < 0.5) { impulseVector.dx = 0.0; }
+    if (ABS(impulseVector.dy) < 0.5) { impulseVector.dy = 0.0; }
     
     // Modify gravity based on accelerometer to move the player
-    CGVector gravityVector = CGVectorMake(self.motion.accelerometerData.acceleration.y * 9.8,
-                                       self.motion.accelerometerData.acceleration.x * -9.8);
+    CGVector gravityVector = CGVectorMake(self.motion.accelerometerData.acceleration.y * -9.8,
+                                       self.motion.accelerometerData.acceleration.x * 9.8);
     
     
     // Apply new forces
@@ -157,8 +197,6 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
 
 - (void)didBeginContact:(SKPhysicsContact *)contact
 {
-    
-    
 //    CGVector collisionImpluse = CGVectorMake(contact.contactNormal.dx * contact.collisionImpulse,
 //                                             contact.contactNormal.dy * contact.collisionImpulse);
 //    [self.player.physicsBody applyImpulse:collisionImpluse atPoint:contact.contactPoint];
